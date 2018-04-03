15ad14adffcd0b9e4c15502df2081e7d380f6d1d
Artem Klepcha <woullbors96@yandex.ru>
2018-04-03 21:39:59 +0300

    task 5(срабатывает клик на куб, на сферу, но по imageTarget нет)

---
+++ CubeProject_part2(sphere)/Assets/Code/MoveScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveScript : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("нажатие");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit r;
+            if (Physics.Raycast(ray, out r, 500))
+            {
+                agent.SetDestination(r.point);
+            }
+        }
+    }
+
+
+    //   [SerializeField]
+    //   Transform destination;
+
+    //   NavMeshAgent navMeshAgent;
+    //// Use this for initialization
+    //void Start () {
+    //       navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+    //       if (navMeshAgent == null)
+    //       {
+    //           Debug.LogError("not isset" + gameObject.name);
+    //       }
+    //       else
+    //       {
+    //           SetDestination();
+    //       }
+    //}
+
+    //   private void SetDestination()
+    //   {
+    //       if (destination != null)
+    //       {
+    //           Vector3 targetVector = destination.transform.position;
+    //           navMeshAgent.SetDestination(targetVector);
+    //       }
+    //   }
+}
