0ba279e2dcd6adc06fd8eaee4dcd3b561582f29f
Artem Klepcha <woullbors96@yandex.ru>
2018-04-24 19:12:25 +0300

    Add Dropdown

---
+++ Assets/Code/ChangeColorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorScript : MonoBehaviour {
+
+    Dropdown colorsDropdown;
+    public Color[] colors;
+    public Renderer renderer;
+
+    void Start()
+    {
+        colorsDropdown = GetComponent<Dropdown>();
+        colorsDropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(colorsDropdown);
+        });
+        DropdownValueChanged(colorsDropdown);
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void DropdownValueChanged(Dropdown change)
+    {
+        renderer.material.color = colors[change.value];
+    }
+}
---
+++ Assets/Code/ChangeFiguresScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeFiguresScript : MonoBehaviour {
+
+    Dropdown figuresDropdown;
+    public Mesh[] meshes;
+    public MeshFilter meshFilter;
+
+    void Start()
+    {
+        figuresDropdown = GetComponent<Dropdown>();
+        figuresDropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(figuresDropdown);
+        });
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void DropdownValueChanged(Dropdown change)
+    {
+        meshFilter.mesh = meshes[change.value];
+    }
+}
